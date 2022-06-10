e9442c8a28745bf6ba10f164dd44142bd98ed4e0
MinBaeBae <msbae92@gmail.com>
2022-06-10 13:17:39 +0900

    finished classes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	let [title, setTitle] = useState([
+		"Best Coffee Spot",
+		"Best Food Spot",
+		"Best Beer Spot",
+	]);
+
+	let [thumbsUp, setThumbsUp] = useState([0, 0, 0]);
+	let [modal, setModal] = useState(false);
+	let [postTitle, setPostTitle] = useState(0);
+
+	return (
+		<div className="App">
+			<div className="black-nav">
+				<div>배log의 포틀랜드 101</div>
+			</div>
+
+			{title.map(function (글제목, i) {
+				return (
+					<div className="list">
+						<h4
+							onClick={() => {
+								setModal(!modal);
+								setPostTitle(i);
+							}}
+						>
+							{글제목}{" "}
+							<span
+								onClick={() => {
+									let copyThumb = [...thumbsUp];
+									copyThumb[i] = copyThumb[i] + 1;
+									setThumbsUp(copyThumb);
+								}}
+							>
+								👍
+							</span>
+							{thumbsUp[i]}
+						</h4>
+						<p>2월 17일 발행</p>
+					</div>
+				);
+			})}
+			{/* <div className="list">
+				<h4>
+					{title[0]}{" "}
+					<span
+						onClick={() => {
+							setThumbsUp(thumbsUp + 1);
+						}}
+					>
+						👍
+					</span>
+					{thumbsUp}
+				</h4>
+				<p>2월 17일 발행</p>
+			</div>
+
+			<div className="list">
+				<h4>
+					{title[1]} <span>👍</span> {thumbsUp}
+				</h4>
+				<p>2월 17일 발행</p>
+			</div>
+			<div className="list">
+				<h4
+					onClick={() => {
+						setModal(!modal);
+					}}
+				>
+					{title[2]} <span>👍</span> {thumbsUp}
+				</h4>
+				<p>2월 17일 발행</p>
+			</div> */}
+
+			{modal == true ? <Modal postTitle={postTitle} title={title} /> : null}
+		</div>
+	);
+}
+
+function Modal(props) {
+	return (
+		<div className="modal">
+			<h4>{props.title[props.postTitle]}</h4>
+			<p>날짜</p>
+			<p>상세내용</p>
+			<button>글 수정</button>
+		</div>
+	);
 }
 
 export default App;
